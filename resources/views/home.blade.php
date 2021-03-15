244ae07240c3fcfc2899b70d1c74866e8d665dc8
Jayclyde Taguines <jayclydetaguines@gmail.com>
2021-03-15 16:06:20 +0800

    second Commit Mar. 15, 2021

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in! sana all') }}
                 </div>
             </div>
         </div>
